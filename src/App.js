4082863c4925eb89d4ad32fbbf7a58e62b0a06cf
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2023-04-13 12:29:46 +0300

    extra-sign-in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './routes/navigation/navigation.component.jsx';
-import Home from './routes/home/home.component.jsx';
 
+import Home from './routes/home/home.component.jsx';
+import Navigation from './routes/navigation/navigation.component.jsx';
+import SignIn from './routes/sign-in/sign-in.component.jsx';
 
 
 const Shop = () => {
   return <h1>I am at the shop page</h1>
-}
+};
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/' element = {<Navigation />}>
         <Route index element = {<Home />} />
         <Route path='shop' element = {<Shop />} />
+        <Route path='sign-in' element = {<SignIn />} />
       </Route>
     </Routes>
   );
