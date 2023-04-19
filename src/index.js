5b4af8085cd7096b5b11ecc78c8b491a078812c1
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2023-04-19 08:34:13 +0300

    additional info

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
