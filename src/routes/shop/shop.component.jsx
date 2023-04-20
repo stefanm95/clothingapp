ba825a0a285d0ccba753a378048bf7ac8cd08185
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2023-04-20 09:11:32 +0300

    product-dropdown styles and updates

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 
 import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-cart/product-cart.component';
+import ProductCard from '../../components/product-card/product-card.component';
 
 import './shop.styles.scss';
 
