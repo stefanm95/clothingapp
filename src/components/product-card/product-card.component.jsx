35e76997515e734428eef172f6f3a8deb921c324
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2023-04-22 16:32:32 +0300

    product-card styles update

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -13,13 +13,14 @@ const ProductCard = ({ product }) => {
     
     return (<div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`}/>
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
+        <div className='footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
+        </div>
+      <Button buttonType='inverted' onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>)
-
 };
 
 export default ProductCard;
