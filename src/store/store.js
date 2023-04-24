9c766065fae2823f86754b59d122122061a9b1ee
stefanm95 <85728264+stefanm95@users.noreply.github.com>
2023-04-24 13:50:43 +0300

    mini-update

--- src/store/store.js
+++ src/store/store.js
@@ -7,20 +7,10 @@ const loggerMiddleware = (store) => (next) => (action) => {
   if(!action.type) {
     return next(action);
   }
-
-  console.log('type: ', action.type)
-  console.log('payload: ', action.payload)
-  console.log('currentState: ', store.getState());
-
   next(action);
-
-  console.log('next state: ', store.getState());
 } ;
 
 const middleWares = [loggerMiddleware];
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-//  Boolean
-// );
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
